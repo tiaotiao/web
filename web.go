b3c05ec3c539e983a81b0b5482aa6bae1a5d2e52
tiaotiao <tiaotiaoyly@gmail.com>
2015-07-29 16:27:01 +0800

    add comments

--- web.go
+++ web.go
@@ -11,8 +11,45 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var HttpReadTimeout = time.Minute
-
+// Web is the main object of the framework. All things start from here.
+// To create an Web object, call NewWeb().
+//
+// You can use this web framework to handle HTTP requests by writing your own functions
+// and return results with JSON format (or other custom format if you want).
+//
+// Note that this framework is designed for API server. To keep it simple, it doesn't support
+// static page, html template, css, javascript etc.
+//
+// Here is an example:
+//
+// 	import "github.com/tiaotiao/web"
+//
+// 	func Hello(c *web.Context) interface{} {
+// 		return "hello world" // return a string will be write directly with out processing.
+// 	}
+//
+// 	func Echo(c *web.Context) interface{} {
+// 		args := struct {
+// 			Message string `web:"message,required"`
+// 		}{}
+//
+//		// scheme args from c.Values into args by the indication of struct tags.
+// 		if err := web.Scheme(c.Values, &args); err != nil {
+// 			return err 	// error occured if lack of argument or wrong type.
+// 		}
+//
+// 		return web.Result{"message": args.Message} // a map or struct will be formated to JSON
+// 	}
+//
+// 	func main() {
+// 		w := web.NewWeb()
+//
+// 		w.Handle("GET", "/api/hello", Hello)
+// 		w.Handle("GET", "/api/echo", Echo)
+//
+// 		w.ListenAndServe("tcp", ":8082")  // block forever until be closed
+// 	}
+//
 type Web struct {
 	mux    *mux.Router
 	router *router
@@ -29,6 +66,7 @@ type Web struct {
 	closed bool
 }
 
+// Create an Web object.
 func NewWeb() *Web {
 	w := new(Web)
 
@@ -45,6 +83,7 @@ func NewWeb() *Web {
 	return w
 }
 
+// ServeHTTP used for implements http.Handler interface. No need to be called by user.
 func (w *Web) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	w.wg.Add(1)
 	if !w.closed {
@@ -57,6 +96,8 @@ func (w *Web) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	w.wg.Done()
 }
 
+// Listen an address and start to serve. Blocked until be closed or some error occurs.
+// See Web.Listen and Web.Serve.
 func (w *Web) ListenAndServe(protocol string, addr string) error {
 	var err error
 
@@ -68,6 +109,12 @@ func (w *Web) ListenAndServe(protocol string, addr string) error {
 	return w.Serve()
 }
 
+// Listen on the local network address. Argument protocol usually be 'tcp' or 'unix'.
+// Argument addr usually be the format as 'host:port'. For example:
+//		Listen("tcp", ":8080")	// listen on 8080 port
+//		Listen("tcp", "google.com:http") // listen on 80 port from google.com
+//		Listen("unix", "/var/run/web_server.sock")
+// See net.Listen for more.
 func (w *Web) Listen(protocol string, addr string) error {
 	var err error
 
@@ -81,8 +128,7 @@ func (w *Web) Listen(protocol string, addr string) error {
 	return nil
 }
 
-// serve all listeners
-// It will block until all listeners closed.
+// Start to serve all listeners. It will block until all listeners closed.
 func (w *Web) Serve() error {
 	var err error
 	if len(w.listeners) == 0 {
@@ -114,6 +160,7 @@ func (w *Web) Serve() error {
 	return err
 }
 
+// Close all listeners and stop serve HTTP.
 func (w *Web) Close() {
 	for _, l := range w.listeners {
 		l.Close()
@@ -124,30 +171,41 @@ func (w *Web) Close() {
 	w.wg.Wait()
 }
 
-func (w *Web) Append(midd Middleware) {
-	w.router.Append(midd)
-}
-
+// Register an WebFunc as a handler for this url. See Router.Handle
 func (w *Web) Handle(method string, path string, fn WebFunc) *MiddlewaresManager {
 	return w.router.Handle(method, path, fn)
 }
 
+// Get a sub router with the perfix path. See Router.SubRouter
 func (w *Web) SubRouter(pathPerfix string) Router {
 	return w.router.SubRouter(pathPerfix)
 }
 
+// Append a middleware. See Router.Append
+func (w *Web) Append(midd Middleware) {
+	w.router.Append(midd)
+}
+
+// To setup a custom responser to process the result which returned from WebFunc and then to write into response body.
+// The responser must implements the Responser interface.
+//
+// With out doing anything, the DefaultResponser will write string and []byte directly or write map, struct and
+// other types in JSON format. See DefaultResponser for more detail.
 func (w *Web) SetResponser(r Responser) {
 	w.responser = r
 }
 
+// Set a logger to track request logs.
 func (w *Web) SetLogger(l Logger) {
 	w.logger = l
 }
 
+// Get statistic.
 func (w *Web) GetStatistic() *Stat {
 	return w.stat
 }
 
+// Get all registed handlers.
 func (w *Web) GetHandlers() map[string]*WebHandler {
 	return w.handlers
 }
@@ -189,6 +247,8 @@ func (w *Web) handle(method, urlpath string, fn WebFunc, midwares *MiddlewaresMa
 func methodUrl(method string, path string) string {
 	return method + " " + strings.ToLower(path)
 }
+
+var HttpReadTimeout = time.Minute
 
 const (
 	POST    = "POST"
